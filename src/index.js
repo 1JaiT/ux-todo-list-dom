8c342718d107268b7f9772e83a7338a1483246b6
Jessica Thurman <jaithurman@gmail.com>
2020-05-04 00:29:29 -0500

    secondcommit

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ let todos = [
         description: "Call the caterers"
     }
 ];
-
 function renderTodoApp() {
     let app = document.querySelector("#app")
     let h1 = document.createElement("h1")
     // your code here
     let ul = document.createElement('ul')
+    
     
     for (let task of todos) {
         let li = document.createElement('li');
@@ -37,7 +37,16 @@ function renderTodoApp() {
        }
        ul.appendChild(li)
     }
-
+    document.getElementById("button").addEventListener("click", function(){
+        let todo = document.getElementById("todo")
+        let newLi= document.createElement("li")
+        newLi.style.listStyle="none"
+        newLi.innerHTML = `<input type="checkbox" id="task" name="task"></input>
+ 
+        <label for="task">${todo.value}</label>`
+        ul.appendChild(newLi)
+        todo.value =""
+    })
     h1.innerText = "Todo List"
 
     app.appendChild(h1)
